1f5577054770e84b4c5836167120fc0a701c526c
Roger E Critchlow Jr <rec@elf.org>
2018-08-14 13:38:20 -0400

    Last of examples, sysex has a link error.

--- linux/examples/sysex.cpp
+++ linux/examples/sysex.cpp
@@ -1,76 +1,14 @@
-#include <Ethernet.h>
+#include <Arduino.h>
+#define F(x) x
+
+#include <LinuxUdp.h>
 
 #include "AppleMidi.h"
-
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
 
 unsigned long t0 = millis();
 bool isConnected = false;
 
-APPLEMIDI_CREATE_DEFAULT_INSTANCE(); // see definition in AppleMidi_Defs.h
-
-// -----------------------------------------------------------------------------
-//
-// -----------------------------------------------------------------------------
-void setup()
-{
-  // Serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-  Serial.print(F("Getting IP address..."));
-
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println();
-    Serial.println(F("Failed DHCP, check network cable & reboot"));
-    for (;;)
-      ;
-  }
-
-  Serial.println();
-  Serial.print(F("IP address is "));
-  Serial.println(Ethernet.localIP());
-
-  Serial.println(F("OK, now make sure you an rtpMIDI session that is Enabled"));
-  Serial.print(F("Add device named Arduino with Host/Port "));
-  Serial.print(Ethernet.localIP());
-  Serial.println(F(":5004"));
-  Serial.println(F("Then press the Connect button"));
-  Serial.println(F("Then open a MIDI listener (eg MIDI-OX) and monitor incoming notes"));
-
-  // Create a session and wait for a remote host to connect to us
-  AppleMIDI.begin("test");
-
-  AppleMIDI.OnConnected(OnAppleMidiConnected);
-  AppleMIDI.OnDisconnected(OnAppleMidiDisconnected);
-
-  AppleMIDI.OnReceiveSysEx(OnAppleMidiSysEx);
-
-  Serial.println(F("Listening for SysEx events"));
-}
-
-// -----------------------------------------------------------------------------
-//
-// -----------------------------------------------------------------------------
-void loop()
-{
-  // Listen to incoming notes
-  AppleMIDI.run();
-
-  // send a sysex msg every 5 seconds
-  if (isConnected && ((millis() - t0) > 5000))
-  {
-    t0 = millis();
-
-    sendSysEx();
-  }
-}
+APPLEMIDI_CREATE_INSTANCE(LinuxUDP, AppleMIDI); // see definition in AppleMidi_Defs.h
 
 // -----------------------------------------------------------------------------
 // rtpMIDI session. Device connected
@@ -142,3 +80,46 @@ char getSysExStatus(const byte* data, uint16_t length)
 }
 
 
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void setup()
+{
+
+  Serial.println(F("OK, now make sure you an rtpMIDI session that is Enabled"));
+  Serial.print(F("Add device named Arduino with Host/Port "));
+  // Serial.print(Ethernet.localIP());
+  Serial.println(F(":5004"));
+  Serial.println(F("Then press the Connect button"));
+  Serial.println(F("Then open a MIDI listener (eg MIDI-OX) and monitor incoming notes"));
+
+  // Create a session and wait for a remote host to connect to us
+  AppleMIDI.begin("test");
+
+  AppleMIDI.OnConnected(OnAppleMidiConnected);
+  AppleMIDI.OnDisconnected(OnAppleMidiDisconnected);
+
+  AppleMIDI.OnReceiveSysEx(OnAppleMidiSysEx);
+
+  Serial.println(F("Listening for SysEx events"));
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void loop()
+{
+  // Listen to incoming notes
+  AppleMIDI.run();
+
+  // send a sysex msg every 5 seconds
+  if (isConnected && ((millis() - t0) > 5000))
+  {
+    t0 = millis();
+
+    sendSysEx();
+  }
+}
+
+int main() { setup(); while(1) loop(); return 0; }
